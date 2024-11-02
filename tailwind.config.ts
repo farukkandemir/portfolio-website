aee75713c43b8b8631ab00aca41106b0ae5dc7f9
farukkandemir <farukkandemir09@gmail.com>
2024-11-02 15:18:16 -0600

    some tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,9 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: ["class"],
   content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./lib/**/*.{ts,tsx}",
-    "./styles/**/*.css",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
